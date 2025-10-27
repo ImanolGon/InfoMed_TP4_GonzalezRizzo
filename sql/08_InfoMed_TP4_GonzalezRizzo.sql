8104261bae7bbfe68fb40de2d0d36ec56b01d406
ImanolGon <167717435+ImanolGon@users.noreply.github.com>
2025-10-27 15:45:07 -0300

    correcion.

--- sql/08_InfoMed_TP4_GonzalezRizzo.sql
+++ sql/08_InfoMed_TP4_GonzalezRizzo.sql
@@ -3,4 +3,4 @@ FROM pacientes
 JOIN sexobiologico ON pacientes.id_sexo = sexobiologico.id_sexo
 GROUP BY ciudad, sexobiologico.descripcion
 ORDER BY ciudad, sexo;
-``` :contentReference[oaicite:10]{index=10}
+
